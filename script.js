297bfe790cf51684f50039abb6813030b5ad9f38
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-07-05 17:39:39 -0700

    got the api to function properly by putting  instead of a specific city name in the api call to return data from any city

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const search = document.getElementById("search");
 
 //do the api call to return the data
 const url = (city) =>
-`http://api.openweathermap.org/data/2.5/weather?q=Berlin&APPID=6fcd46ca0bc9014cd6e642c6441a435a`
+`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=6fcd46ca0bc9014cd6e642c6441a435a`
 
 //function to tell the app what to do with the json returned data 
 async function getWeatherByLocation(city) {
@@ -19,8 +19,33 @@ async function getWeatherByLocation(city) {
 }
 
 //function to put the data onto a spot on the page and generate html
-function addWeatherToPage(data)
+function addWeatherToPage(data) {
+    const temp = Ktoc(data.main.temp);
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
+    //modify the html from js so that data goes in the browser window 
+    weather.innerHTML = `<h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> 
+    ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/></h2>
+    <small>${data.weather[0].main}</small>`;
 
- 
+    //make sure it's blank so data is displayed  
+    main.innerHTML = "";
+    //append the child element 
+    main.appendChild(weather);
+}
+
+//call the Ktoc const that was established above 
+function Ktoc(K) {
+    return Math.floor(K - 273.15);
+};
+
+//add the event listener so the search button is interactive 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const city = search.value;
+    if(city) {
+        getWeatherByLocation(city);
+    }
+});
 
 
