a328360b62ad1efea16fa196438892cbade98321
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-07-05 17:25:55 -0700

    got the api call working i think? Had to browse the internet for solutions because my key kept saying invalid but I think its working now

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-api link 
+//declare variables to reference later 
+const apikey = "6fcd46ca0bc9014cd6e642c6441a435a";
 
-https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-api key 
-6fcd46ca0bc9014cd6e642c6441a435a
+//do the api call to return the data
+const url = (city) =>
+`http://api.openweathermap.org/data/2.5/weather?q=Berlin&APPID=6fcd46ca0bc9014cd6e642c6441a435a`
+
+//function to tell the app what to do with the json returned data 
+async function getWeatherByLocation(city) {
+    const resp = await fetch(url(city), { origin: "cors" });
+    const respData = await resp.json();
+    //console log the data to show it returns something in devtools 
+    console.log(respData);
+    addWeatherToPage(respData);
+}
+
+//function to put the data onto a spot on the page and generate html
+function addWeatherToPage(data)
+
+ 
+
+
